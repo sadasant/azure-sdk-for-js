1d6cd0d40fcaf046020b839ca1922d84d4693870
Daniel Rodríguez <sadasant@gmail.com>
2020-08-27 18:10:37 -0400

    wip

---
+++ sdk/keyvault/keyvault-admin/test/public/accessControlClient.spec.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as assert from "assert";
+import { env, Recorder } from "@azure/test-utils-recorder";
+import { AbortController } from "@azure/abort-controller";
+
+describe("KeyVaultAccessControlClient", () => {
+  it("listRoleDefinitions", async function() {
+  });
+  it("createRoleAssignment", async function() {
+  });
+  it("getRoleAssignment", async function() {
+  });
+  it("deleteRoleAssignment", async function() {
+  });
+});
