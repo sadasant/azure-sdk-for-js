e2cfc83d2e09b2385b8957efa902672511fb90aa
Daniel Rodríguez <sadasant@gmail.com>
2020-08-27 15:18:31 -0400

    backupClient done?

--- sdk/keyvault/keyvault-admin/src/lro/selectiveRestore/poller.ts
+++ sdk/keyvault/keyvault-admin/src/lro/selectiveRestore/poller.ts
@@ -3,11 +3,16 @@
 
 import { delay, RequestOptionsBase } from "@azure/core-http";
 import { Poller } from "@azure/core-lro";
-import { KeyVaultClient } from '../../generated/keyVaultClient';
-import { RestoreOperationState, RestorePollOperationState, makeRestorePollOperation } from "./operation";
+import { KeyVaultClient } from "../../generated/keyVaultClient";
+import {
+  SelectiveRestoreOperationState,
+  SelectiveRestorePollOperationState,
+  makeSelectiveRestorePollOperation
+} from "./operation";
 
-export interface RestorePollerOptions {
+export interface SelectiveRestorePollerOptions {
   client: KeyVaultClient;
+  keyName: string;
   vaultUrl: string;
   blobStorageUri: string;
   sasToken: string;
@@ -18,26 +23,37 @@ export interface RestorePollerOptions {
 }
 
 /**
- * Class that creates a poller that waits until a Key Vault ends up being restored.
+ * Class that creates a poller that waits until a key of a Key Vault backup ends up being restored.
  */
-export class RestorePoller extends Poller<RestorePollOperationState, undefined> {
+export class SelectiveRestorePoller extends Poller<SelectiveRestorePollOperationState, undefined> {
   /**
    * Defines how much time the poller is going to wait before making a new request to the service.
-   * @memberof RestorePoller
+   * @memberof SelectiveRestorePoller
    */
   public intervalInMs: number;
 
-  constructor(options: RestorePollerOptions) {
-    const { client, vaultUrl, blobStorageUri, sasToken, folderName, requestOptions, intervalInMs = 2000, resumeFrom } = options;
+  constructor(options: SelectiveRestorePollerOptions) {
+    const {
+      client,
+      vaultUrl,
+      keyName,
+      blobStorageUri,
+      sasToken,
+      folderName,
+      requestOptions,
+      intervalInMs = 2000,
+      resumeFrom
+    } = options;
 
-    let state: RestorePollOperationState | undefined;
+    let state: SelectiveRestorePollOperationState | undefined;
 
     if (resumeFrom) {
       state = JSON.parse(resumeFrom).state;
     }
 
-    const operation = makeRestorePollOperation({
+    const operation = makeSelectiveRestorePollOperation({
       ...state,
+      keyName,
       blobStorageUri,
       sasToken,
       folderName,
@@ -53,7 +69,7 @@ export class RestorePoller extends Poller<RestorePollOperationState, undefined> 
 
   /**
    * The method used by the poller to wait before attempting to update its operation.
-   * @memberof RestorePoller
+   * @memberof SelectiveRestorePoller
    */
   async delay(): Promise<void> {
     return delay(this.intervalInMs);
@@ -62,10 +78,11 @@ export class RestorePoller extends Poller<RestorePollOperationState, undefined> 
   /**
    * Gets the public state of the polling operation
    */
-  public getOperationState(): RestoreOperationState {
-    const state: RestoreOperationState = this.operation.state;
+  public getOperationState(): SelectiveRestoreOperationState {
+    const state: SelectiveRestoreOperationState = this.operation.state;
     return {
       vaultUrl: state.vaultUrl,
+      keyName: state.keyName,
       blobStorageUri: state.blobStorageUri,
       sasToken: state.sasToken,
       folderName: state.folderName,
