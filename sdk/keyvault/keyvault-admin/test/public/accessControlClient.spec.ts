d1a56974ea85a2a10bfce83438feccbd3c851ed3
Daniel Rodríguez <sadasant@gmail.com>
2020-08-28 08:59:32 -0400

    more test related stuff

--- sdk/keyvault/keyvault-admin/test/public/accessControlClient.spec.ts
+++ sdk/keyvault/keyvault-admin/test/public/accessControlClient.spec.ts
@@ -1,12 +1,37 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as assert from "assert";
-import { env, Recorder } from "@azure/test-utils-recorder";
-import { AbortController } from "@azure/abort-controller";
+// import * as assert from "assert";
+import { Recorder } from "@azure/test-utils-recorder";
+// import { AbortController } from "@azure/abort-controller";
+
+import { KeyVaultAccessControlClient } from '../../src';
+// import { assertThrowsAbortError } from "../utils/common";
+// import { testPollerProperties } from "../utils/recorder";
+import { authenticate } from "../utils/authentication";
 
 describe("KeyVaultAccessControlClient", () => {
+  // const prefix = "RBAC";
+  // let suffix: string;
+  let client: KeyVaultAccessControlClient;
+  let recorder: Recorder;
+
+  beforeEach(async function() {
+    const authentication = await authenticate(this);
+    // suffix = authentication.suffix;
+    client = authentication.client;
+    recorder = authentication.recorder;
+  });
+
+  afterEach(async function() {
+    await recorder.stop();
+  });
+
+  // The tests follow
   it("listRoleDefinitions", async function() {
+    for await (const roleDefinitions of client.listRoleDefinitions("/")) {
+      console.log("Role definition: ", roleDefinitions);
+    }
   });
   it("createRoleAssignment", async function() {
   });
---
+++ sdk/keyvault/keyvault-admin/test/utils/authentication.ts
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { ClientSecretCredential } from "@azure/identity";
+import { env, record, RecorderEnvironmentSetup } from "@azure/test-utils-recorder";
+
+import { KeyVaultAccessControlClient } from '../../src';
+import { getKeyvaultName } from "./common";
+import { uniqueString } from "./recorder";
+
+export async function authenticate(that: any): Promise<any> {
+  const secretSuffix = uniqueString();
+  const recorderEnvSetup: RecorderEnvironmentSetup = {
+    replaceableVariables: {
+      AZURE_CLIENT_ID: "azure_client_id",
+      AZURE_CLIENT_SECRET: "azure_client_secret",
+      AZURE_TENANT_ID: "azure_tenant_id",
+      KEYVAULT_NAME: "keyvault_name"
+    },
+    customizationsOnRecordings: [
+      (recording: any): any =>
+        recording.replace(/"access_token":"[^"]*"/g, `"access_token":"access_token"`),
+      (recording: any): any =>
+        secretSuffix === "" ? recording : recording.replace(new RegExp(secretSuffix, "g"), "")
+    ],
+    queryParametersToSkip: []
+  };
+  const recorder = record(that, recorderEnvSetup);
+  const credential = await new ClientSecretCredential(
+    env.AZURE_TENANT_ID,
+    env.AZURE_CLIENT_ID,
+    env.AZURE_CLIENT_SECRET
+  );
+
+  const keyVaultName = getKeyvaultName();
+  const keyVaultUrl = `https://${keyVaultName}.vault.azure.net`;
+  const client = new KeyVaultAccessControlClient(keyVaultUrl, credential);
+
+  return { recorder, client, secretSuffix };
+}
