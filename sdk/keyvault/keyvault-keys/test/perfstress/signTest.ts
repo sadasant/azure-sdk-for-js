7d287128d00db1fbaa76a574b74c3263e82738b7
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-12-17 00:52:36 +0000

    wip

--- sdk/keyvault/keyvault-keys/test/perfstress/signTest.ts
+++ sdk/keyvault/keyvault-keys/test/perfstress/signTest.ts
@@ -9,6 +9,8 @@ import { ClientSecretCredential, TokenCredential } from "@azure/identity";
 
 dotenv.config();
 
+// npm run perfstress-test:node -- SignTest --warmup 30 --duration 20 --iterations 120 --milliseconds-to-log 10000
+
 interface SignTestOptions {
   valueSize: number;
   keyName: string;
@@ -33,6 +35,7 @@ export class SignTest extends PerfStressTest {
   public value: string;
   public keyName: string;
   public keyVaultKey: KeyVaultKey | undefined;
+  public total = 0;
 
   constructor() {
     super();
@@ -52,6 +55,18 @@ export class SignTest extends PerfStressTest {
 
 
   public async globalSetup(): Promise<void> {
+    try {
+      const deletedKey = await this.keyClient.getDeletedKey(this.keyName);
+      await this.keyClient.purgeDeletedKey(deletedKey.name);
+    } catch (e) {
+    }
+    try {
+      const key = await this.keyClient.getKey(this.keyName);
+      const deletePoller = await this.keyClient.beginDeleteKey(key.name);
+      await deletePoller.pollUntilDone();
+      await this.keyClient.purgeDeletedKey(key.name);
+    } catch (e) {
+    }
     this.keyVaultKey = await this.keyClient.createKey(this.keyName, "RSA");
   }
 
@@ -61,11 +76,23 @@ export class SignTest extends PerfStressTest {
     await this.keyClient.purgeDeletedKey(this.keyName);
   }
 
+  public setup() {
+    console.log(process.memoryUsage().heapUsed);
+  }
+
+  public cleanup() {
+    console.log(process.memoryUsage().heapUsed);
+  }
+
   async runAsync(): Promise<void> {
     const hash = createHash("sha256");
     hash.update(this.value!);
     const digest = hash.digest();
     const cryptoClient = new CryptographyClient(this.keyVaultKey!.id!, this.credential!);
     await cryptoClient.sign("RS256", digest);
+    if (this.total % 500) {
+      console.log(process.memoryUsage().heapUsed);
+    }
+    this.total++;
   }
 }
